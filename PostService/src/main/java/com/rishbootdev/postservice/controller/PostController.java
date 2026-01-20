0965ed184e731d6a5c41a5668ac3f240ecc58a50
Rishabh <rishabhdubey1472004@gmail.com>
2026-01-20 09:31:51 +0530

    configured services for eureka discovery

--- PostService/src/main/java/com/rishbootdev/postservice/controller/PostController.java
+++ PostService/src/main/java/com/rishbootdev/postservice/controller/PostController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/core")
+@RequestMapping("/post")
 @RequiredArgsConstructor
 public class PostController {
 
