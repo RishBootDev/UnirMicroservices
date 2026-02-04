c39ae103d7b56d6e85b32d55844413223e1b2632
Rishabh <rishabhdubey1472004@gmail.com>
2026-02-04 19:47:52 +0530

    fixed the subscription service in the unir app

--- SubscriptionService/src/main/java/com/rishbootdev/subscriptionservice/auth/CorsConfig.java
+++ SubscriptionService/src/main/java/com/rishbootdev/subscriptionservice/auth/CorsConfig.java
@@ -1,9 +1,6 @@
 package com.rishbootdev.subscriptionservice.auth;
 
 
-
-
-
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
